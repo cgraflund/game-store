a2e314fe0906ca5f751eaddca8d81beca4a5bc47
cgraflund <connor.graf@gmail.com>
2021-10-25 13:21:33 -0600

    Initial commit.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="/welcome.css">
+</head>
+
+<body>
+    <?php include 'Unit1_header.php';?>
+    <?php include 'Unit1_store.php';?>
+    <?php include 'Unit1_footer.php';?>
+</body>
